9cc3b8cf01387dcb9ec2a8b608221066608303db
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-09-09 22:10:27 -0300

    Platzi market project

---
+++ src/main/java/com/platzi/market/persistence/entities/Cliente.java
@@ -0,0 +1,69 @@
+package com.platzi.market.persistence.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    @Id
+    @Column(name = "id")
+    private String idCliente;
+
+    private String nombre;
+    private String apellido;
+    private Integer celular;
+    private String direccion;
+    @Column(name = "correo_electronico")
+    private String email;
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Integer celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
