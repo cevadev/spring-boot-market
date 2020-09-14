cfb6049752d6e3df1e076c543047706417089f48
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-09-14 15:28:10 -0300

    Platzi market project

---
+++ src/main/java/com/platzi/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,15 @@
+package com.platzi.market.domain.repository;
+
+import com.platzi.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+
+    //usamos optional ya que puede ser que el cliente consultado no hay realizado una compra
+    Optional<List<Purchase>> getByClient(int clientId);
+
+    Purchase save(Purchase purchase);
+}
